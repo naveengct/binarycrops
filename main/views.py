4cc9be882ebc08e310a4d92241ddf9220b0fa330
CHOUTHRI <naveenrajesh2222@gmail.com>
2020-04-21 14:47:05 +0530

    okay

--- main/views.py
+++ main/views.py
@@ -13,11 +13,9 @@ def home(request):
 def hello(request):
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     MODEL_PATH = 'breast_cancer.h5'
-    model = models.load_model(os.path.join(BASE_DIR,'main/breast_cancer.h5'),custom_objects={'KerasLayer': hub.KerasLayer})
+    model = models.load_model(os.path.join(BASE_DIR,'main/model_vegetable.h5'),custom_objects={'KerasLayer': hub.KerasLayer})
     img = image.img_to_array(image.load_img(request.FILES.get('img'), target_size=(224, 224, 3))) / 255
     img=np.reshape(img,(1,224,224,3))
-    y=model.predict(img)
-    for i in y:
-      temp1=round(i[0]*100,2)
-      temp2=round(i[1]*100,2)
-    return render(request, 'base.html', {'res1':temp1,'res2':temp2})
+    temp=model.predict(img)
+    y=np.argmax(temp)
+    return render(request, 'base.html', {'res1':CAT[y]})
